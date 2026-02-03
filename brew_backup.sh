8fbe044da2db9c6e8f9a3900806c4d8f151e34d6
Scott Adams <scott.adams@neuralfoundry.co.uk>
2026-02-03 12:53:15 +0000

    Regular automated backup [skip ci]

--- brew_backup.sh
+++ brew_backup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# Load your user path so the script can find 'brew' and 'git'
+export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+# Explicitly load Homebrew environment
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo "Homebrew Back-Uperer"
 
 # change to my backup location
-cd $HOME/homebrew
+cd $HOME/.homebrew
 
 # run the backup
 /opt/homebrew/bin/brew bundle dump --force
